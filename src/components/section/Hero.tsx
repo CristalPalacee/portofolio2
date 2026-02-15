de83423f9dfb135d2a54aa128b41e5eb49fca94f
Cristal Palace <rnopal698@gmail.com>
2026-02-15 18:52:29 +0700

    fix

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { ArrowRight, Github, Linkedin } from "lucide-react";
 import { PROFILE } from "@/lib/data";
 import ShinyText from "../ui/ShinyText";
-import { filter } from "framer-motion/client";
+
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
@@ -14,7 +14,7 @@ const fadeInUp = {
 
 const staggerContainer = {
   visible: { transition: { staggerChildren: 0.5 },
-          filter: { blur: 3, opacity: 1 },
+        
 },
 
 };
